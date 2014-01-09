8718926b6ecf463e869a2b36cab0211819b87359
Martin Sturm <msturm@ebay.com>
2014-01-09 15:55:33 +0100

    Fix download link again

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@
 class chicken_of_the_vnc {
   package { 'Chicken_of_the_vnc':
     ensure   => installed,
-    source   => 'http://downloads.sourceforge.net/project/chicken/Chicken-2.2b2.dmg?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fchicken%2Ffiles%2F%3Fsource%3Dnavbar&ts=1389278671&use_mirror=heanet',
-    provider => 'pkgdmg'
+    source   => 'http://downloads.sourceforge.net/chicken/Chicken-2.2b2.dmg',
+    provider => 'appdmg'
   }
 }
