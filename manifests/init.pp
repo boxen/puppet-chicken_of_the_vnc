d35ae704942a9b3e99f815b6c97c56d85b0bdc93
Martin Sturm <msturm@ebay.com>
2014-01-09 15:48:46 +0100

    Fix downloadlink

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@
 class chicken_of_the_vnc {
   package { 'Chicken_of_the_vnc':
     ensure   => installed,
-    source   => 'http://downloads.sourceforge.net/project/chicken/Chicken-2.1.1/Chicken-2.1.1.dmg?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fchicken%2Ffiles%2FChicken-2.1.1%2F&ts=1389276351&use_mirror=optimate',
+    source   => 'http://downloads.sourceforge.net/project/chicken/Chicken-2.2b2.dmg?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fchicken%2Ffiles%2F%3Fsource%3Dnavbar&ts=1389278671&use_mirror=heanet',
     provider => 'pkgdmg'
   }
 }
